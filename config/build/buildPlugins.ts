5438b4a041b6ae5b5a46ae62344abafbae0a6c84
Semen Kliuev <penekkakti@gmail.com>
2024-10-18 02:12:37 +0300

    fix plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -5,9 +5,9 @@ import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
-    return [
+    const plugins = [  
         new HtmlWebpackPlugin({
-            template: paths.html,
+        template: paths.html,
         }),
         new webpack.ProgressPlugin(),
         new MiniCssExtractPlugin({
@@ -17,9 +17,12 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
         }),
-        new webpack.HotModuleReplacementPlugin(),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false,
-        }),
-    ];
+    ]
+
+    if (isDev) {plugins.push(new webpack.HotModuleReplacementPlugin())
+        plugins.push(new BundleAnalyzerPlugin({openAnalyzer: false,}))}
+    
+    
+
+    return plugins;
 }
